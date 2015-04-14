cc64174490530c835917b08a5c7e8ba8c3a49193
shiv1994 <shivramx@gmail.com>
2015-04-13 22:19:41 -0400

    Health Summary
    Health Summary along with its processing file.

---
+++ pages/create_health_summary.php
@@ -0,0 +1,297 @@
+<?php require_once("../includes/sessions.php"); ?>
+<?php require_once("../includes/functions.php"); ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>New Health Summary</title>
+
+        <!-- Bootstrap Core CSS -->
+        <link href="../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- MetisMenu CSS -->
+        <link href="../bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
+
+        <!-- Timeline CSS -->
+        <link href="../dist/css/timeline.css" rel="stylesheet">
+
+        <!-- Custom CSS -->
+        <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
+
+        <!-- Morris Charts CSS -->
+        <link href="../bower_components/morrisjs/morris.css" rel="stylesheet">
+
+        <!-- Custom Fonts -->
+        <link href="../bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <script src="../includes/tablejs.js"></script>
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+	</head>
+    
+    <body> 
+        <div id="wrapper">
+        <!-- Navigation -->
+        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">Electronic Health Record System v1.0</a>
+            </div>
+            <!-- /.navbar-header -->
+            <ul class="nav navbar-top-links navbar-right">
+			
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-user">
+                        <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+                        </li>
+                        <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li><a href="login.html"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                        </li>
+                    </ul>
+                    <!-- /.dropdown-user -->
+                </li>
+                <!-- /.dropdown -->
+            </ul>
+            
+            <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <li>
+                            <a href="doctor_dashboard.html"><i class="fa fa-dashboard fa-fw"></i>Doctor Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="tables.html"><i class="fa fa-table fa-fw"></i>Search Patients</a>
+                        </li>
+                        <li>
+                            <a href="forms.html"><i class="fa fa-edit fa-fw"></i>View All Patients</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
+            
+        </nav>   
+            
+        <div id="page-wrapper">
+                <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Add New Health Summary</h1>
+                </div>
+                <!-- /.col-lg-12 -->
+                </div>
+                <!-- /.row -->
+			
+			<?php 
+				
+				if(isset($_SESSION["alert"]))
+				{
+					echo displayAlert();
+				}
+				
+				if(isset($_SESSION["errors"]))
+				{
+					echo displayErrors();
+				}
+				?>
+                <form action="process_health_summary.php" class="register" method="POST">
+                <br>
+                    <div class="col-lg-12">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                Medication Information
+                            </div>
+                            <div class="panel-body">
+
+                            <button type="button" class="btn btn-primary" onClick="addRow('datatable',10)" >Add Medication Record</button>
+                            <button type="button" class="btn btn-primary" onClick="deleteRow('datatable')">Remove Medication Record</button>
+
+                            <br />
+                            <br/>
+
+                                <div class="table-responsive">
+                                    <table class="table table-striped table-bordered table-hover" id="datatable">
+                                        <thead>
+                                            <tr>
+                                                <th>Medication Name</th>
+                                                <th>Dosage</th>
+                                                <th>Clinical Indication</th>
+                                                <th>Comments</th>
+                                                <th>Date Prescribed</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>
+                                                    <input class="form-control"  name="med_medication_name[]">
+                                                </td>
+                                                <td>
+                                                    <input class="form-control" name="med_dosage[]">
+                                                </td>
+                                                <td>
+                                                    <input class="form-control" name="med_clinical_indication[]">
+                                                </td>
+                                                <td>
+                                                    <input class="form-control" name="med_comments[]">
+                                                </td>
+                                                <td>
+                                                    <input class="form-control" type ="date" name="med_date_prescribed[]">
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                    </div>
+                    </div>
+
+                      <br>   
+
+                    <div class="col-lg-12">
+					<div class="panel panel-primary">
+						<div class="panel-heading">
+							Immunization Information
+						</div>
+						<div class="panel-body">
+							
+						<button type="button" class="btn btn-primary" onClick="addRow('datatable2',10)" >Add Immunization Record</button>
+						<button type="button" class="btn btn-primary" onClick="deleteRow('datatable2')">Remove Immunization Record</button>
+						
+						<br />
+						<br/>
+						
+                            <div class="table-responsive">
+                                <table class="table table-striped table-bordered table-hover" id="datatable2">
+                                    <thead>
+                                        <tr>
+                                            <th>Disease</th>
+                                            <th>Vaccine</th>
+                                            <th>Date of Immunization</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>
+												<input class="form-control"  name="immun_disease[]">
+											</td>
+                                            <td>
+												<input class="form-control" name="immun_vaccine[]">
+											</td>
+
+                                            <td>
+												<input class="form-control" type="date" name="immun_date_prescribed[]">
+											</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+				    </div>
+				    </div>
+                    </div>
+
+                    <br>
+
+                    <div class="col-lg-12">
+					<div class="panel panel-primary">
+						<div class="panel-heading">
+							Adverse Reaction
+						</div>
+						<div class="panel-body">
+							
+						<button type="button" class="btn btn-primary" onClick="addRow('datatable3',10)" >Add Adverse Reaction Record</button>
+						<button type="button" class="btn btn-primary" onClick="deleteRow('datatable3')">Remove Adverse Reaction Record</button>
+						
+						<br />
+						<br/>
+						
+                            <div class="table-responsive">
+                                <table class="table table-striped table-bordered table-hover" id="datatable3">
+                                    <thead>
+                                        <tr>
+                                            <th>Substance</th>
+                                            <th>Reaction</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>
+												<input class="form-control"  name="adv_substance[]">
+											</td>
+                                            <td>
+												<input class="form-control" name="adv_reaction[]">
+											</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+						</div>
+					</div>
+                    </div>
+
+                    <br>
+
+                    <div class="col-lg-12">
+					<div class="panel panel-primary">
+						<div class="panel-heading">
+							Medical History
+						</div>
+						<div class="panel-body">
+							
+						<button type="button" class="btn btn-primary" onClick="addRow('datatable4',10)" >Add Medical History Record</button>
+						<button type="button" class="btn btn-primary" onClick="deleteRow('datatable4')">Remove Medical History Record</button>
+						
+						<br />
+						<br/>
+						
+                            <div class="table-responsive">
+                                <table class="table table-striped table-bordered table-hover" id="datatable4">
+                                    <thead>
+                                        <tr>
+                                            <th>Diagnosis</th>
+                                            <th>Date of Onset</th>
+                                            <th>Comment</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>
+												<input class="form-control"  name="hist_diagnosis[]">
+											</td>
+                                            <td>
+												<input class="form-control" type="date" name="hist_comment[]">
+											</td>
+                                            <td>
+												<input class="form-control" name="hist_date_prescribed[]">
+											</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+						</div>
+					</div>
+                    </div>
+                    <br>
+			        <button type="submit" class="btn btn-primary" name="submit">Add New Health Summary</button>
+        </form>
+        <br>
+        </div>
+    </div>
+    </body>
+</html>
---
+++ pages/process_health_summary.php
@@ -0,0 +1,152 @@
+<?php require_once("../includes/sessions.php"); ?>
+<?php require_once("../includes/database_connection.php"); ?>
+<?php require_once("../includes/functions.php"); ?>
+<?php require_once("../includes/validations.php"); ?>
+
+<?php
+	/*
+		Check to see if a post request was made. If so, we can obtain data and validate it and then store it in the database.
+		If not, the use will be redirected to a blank health summary page.
+	*/
+	if(isset($_POST["submit"]))
+	{
+        //For passing of patientID.
+        //$patientID = $_POST['patientID'];
+        
+        //Remove next line once linked to POST from create document!
+        $patientID=1;
+
+        //PULL DATA FROM MEDICATION TABLE
+        $med_name= $_POST['med_medication_name']; 
+        $med_dosage = $_POST['med_dosage'];
+        $med_clinical = $_POST['med_clinical_indication'];
+        $med_comments = $_POST['med_comments'];
+        $med_date_prescribed = $_POST['med_date_prescribed'];
+
+        $med_name = array_map('prepare_string', $med_name);
+        $med_dosage = array_map('prepare_string', $med_dosage);
+        $med_clinical = array_map('prepare_string', $med_clinical);
+        $med_comments = array_map('prepare_string', $med_comments);
+        $med_date_prescribed = array_map('prepare_string', $med_date_prescribed);
+
+        validate_all_has_presences_med($med_name, $med_dosage, $med_clinical, $med_date_prescribed);
+        
+        
+        //PULL DATA FROM IMMUNIZATION TABLE
+        $immun_disease = $_POST['immun_disease']; 
+        $immun_vaccine = $_POST['immun_vaccine'];
+        $immun_date_prescribed = $_POST['immun_date_prescribed'];
+        
+        
+        $immun_disease = array_map('prepare_string', $immun_disease);
+        $immun_vaccine = array_map('prepare_string', $immun_vaccine);
+        $immun_date_prescribed = array_map('prepare_string', $immun_date_prescribed);
+        
+        validate_all_has_presences_immun($immun_disease,  $immun_vaccine, $immun_date_prescribed);
+        
+        //PULL DATA FROM REACTIONS TABLE
+        $adv_substance = $_POST['adv_substance']; 
+        $adv_reaction = $_POST['adv_reaction'];
+        
+        $adv_substance = array_map('prepare_string', $adv_substance);
+        $adv_reaction = array_map('prepare_string', $adv_reaction);
+        
+        validate_all_has_presences_reactions_health_summary($adv_substance, $adv_reaction);
+        
+        //PULL DATA FROM HISTORY TABLE
+        $hist_diag = $_POST['hist_diagnosis'];
+        $hist_comment = $_POST['hist_comment']; 
+        $hist_date = $_POST['hist_date_prescribed'];
+        
+        $hist_diag = array_map('prepare_string', $hist_diagnosis);
+        $hist_comment = array_map('prepare_string', $hist_comment);
+        $hist_date = array_map('prepare_string', $hist_date);
+        
+        validate_all_has_presences_hist($hist_diag, $hist_date );
+        
+        if(!empty($errors))
+		{
+			$_SESSION["errors"] = $errors;
+			redirect("process_health_summary.php");
+        }
+        else{
+             foreach($med_name as $pos =>$data){
+                $add_Medication_Query = "INSERT INTO MEDICATION (Medicine , Dosage , Indication , Comments , DatePrescribed , PatientID)";
+                $add_Medication_Query .= "VALUES('{$med_name[$pos]}','{$med_dosage[$pos]}','{$med_clinical[$pos]}','{$med_comments[$pos]}','{$med_date_prescribed[$pos]}' ,'{$patientID}' );"; 
+                $result = mysqli_query($connection,$add_Medication_Query);
+                if($result)
+                {
+                    $_SESSION["alert"] ="Rows in Medication table were successfully added.";
+                }
+                else
+                {
+                    $_SESSION["alert"] = "Rows in Medication table were not successfully added.";
+                    redirect("create_health_summary.php");
+                }
+             }
+            
+            foreach($immun_disease as $pos =>$data){
+                $add_Immunization_Query="INSERT INTO IMMUNIZATION (Disease , Vaccine , DateImmunized , PatientID)";
+                $add_Immunization_Query.="VALUES('{$immun_disease[$pos]}','{$immun_vaccine[$pos]}','{$immun_date_prescribed[$pos]}','{$patientID}'); ";
+                $result = mysqli_query($connection,$add_Immunization_Query);
+                if($result)
+                {
+                    $_SESSION["alert"] ="Rows in Immunization table were successfully added.";
+                }
+                else
+                {
+                    $_SESSION["alert"] = "Rows in Immunization table were not successfully added.";
+                    redirect("create_health_summary.php");
+                }
+             }
+            
+            foreach($adv_substance as $pos =>$data){
+                $add_Reaction_Query="INSERT INTO ADVERSEREACTION (Substance , Manifestation , PatientID)";
+                $add_Reaction_Query.=" VALUES('{$adv_substance[$pos]}','{$adv_reaction[$pos]}','{$patientID}')";
+                $result = mysqli_query($connection,$add_Reaction_Query);
+                if($result)
+                {
+                    $_SESSION["alert"] ="Rows in Adverse Reaction table were successfully added.";
+                }
+                else
+                {
+                    $_SESSION["alert"] = "Rows in Adverse Reaction were not successfully added.";
+                    redirect("create_health_summary.php");
+                }
+             }
+            
+            foreach($hist_diag as $pos =>$data){
+                $add_History_Query = "INSERT INTO DIAGNOSIS (Diagnosis , DateOfOnSet , Comments , PatientID)";
+                $add_History_Query .= "VALUES ('{$hist_diag[$pos]}','{$hist_date[$pos]}','{$hist_comment[$pos]}','{$patientID}') ";
+                $result = mysqli_query($connection,$add_History_Query);
+                if($result)
+                {
+                    $_SESSION["alert"] ="Rows in Diagnosis table were successfully added.";
+                }
+                else
+                {
+                    $_SESSION["alert"] = "Rows in Diagnosis were not successfully added.";
+                    redirect("create_health_summary.php");
+                }
+             }
+            
+             redirect("create_health_summary.php");
+            
+        }  
+	}
+	else
+	{
+		redirect("create_health_summary.php");
+	}
+?>
+    
+<?php
+	/*
+		Close database connection.
+	*/
+
+	if(isset($connection))
+	{
+		mysqli_close($connection);
+	}
+?>

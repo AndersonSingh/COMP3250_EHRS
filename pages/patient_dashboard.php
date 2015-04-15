921f197dd158f81684888c4ab89e266040d58de7
shiv1994 <shivramx@gmail.com>
2015-04-14 23:16:27 -0400

    Made Queries for patient dashboard
    Queries allowed the ID of the patient , the amount of doctors
    associated, the total number of events as well as the existence of the
    health summary.

--- pages/patient_dashboard.php
+++ pages/patient_dashboard.php
@@ -1,6 +1,11 @@
 <?php
 	require_once('../includes/patient_session.php');
+	require_once("../includes/database_connection.php"); 
 ?>
+
+<?php   
+	$patientID=$_SESSION["patient_login"];
+?> 
 
 <!DOCTYPE html>
 <html lang="en">
@@ -131,14 +136,18 @@
                                     <i class="fa fa-tasks fa-5x"></i>
                                 </div>
                                 <div class="col-xs-9 text-right">
-                                    <div class="huge">26</div>
-                                    <div>Total Patients</div>
+                                    <div class="huge">
+									<?php
+										echo $patientID;
+									?>
+									</div>
+                                    <div>Patient ID</div>
                                 </div>
                             </div>
                         </div>
                         <a href="#">
                             <div class="panel-footer">
-                                <span class="pull-left">Total Patients</span>
+                                <span class="pull-left">Patient ID</span>
                                 <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
                                 <div class="clearfix"></div>
                             </div>
@@ -153,14 +162,23 @@
                                     <i class="fa fa-tasks fa-5x"></i>
                                 </div>
                                 <div class="col-xs-9 text-right">
-                                    <div class="huge">12</div>
-                                    <div>Total Doctors</div>
+                                    <div class="huge">
+									<?php   
+										$num_doctors_Query = "SELECT doctorID FROM HealthSummary WHERE patientID= $patientID";
+										$result = mysqli_query($connection, $num_doctors_Query);
+										$num_doctors_Query2 = "SELECT DISTINCT doctorID FROM Event WHERE patientID= $patientID";
+										$result2 = mysqli_query($connection, $num_doctors_Query2);
+										$row_cnt = $result ->num_rows + $result2 ->num_rows;
+										echo $row_cnt;
+									?>
+									</div>
+                                    <div>Total Doctors Associated To</div>
                                 </div>
                             </div>
                         </div>
                         <a href="#">
                             <div class="panel-footer">
-                                <span class="pull-left">Total Doctors</span>
+                                <span class="pull-left">Total Doctors Associated To</span>
                                 <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
                                 <div class="clearfix"></div>
                             </div>
@@ -175,7 +193,14 @@
                                     <i class="fa fa-tasks fa-5x"></i>
                                 </div>
                                 <div class="col-xs-9 text-right">
-                                    <div class="huge">124</div>
+                                    <div class="huge">
+									<?php   
+										$num_events_Query = "SELECT eventID FROM Event WHERE patientID= $patientID";
+										$result = mysqli_query($connection, $num_events_Query);
+										$row_cnt = $result ->num_rows;
+										echo $row_cnt;
+									?>
+									</div>
                                     <div>Total Events</div>
                                 </div>
                             </div>
@@ -197,14 +222,24 @@
                                     <i class="fa fa-tasks fa-5x"></i>
                                 </div>
                                 <div class="col-xs-9 text-right">
-                                    <div class="huge">13</div>
-                                    <div>Total Discharges</div>
+                                    <div class="huge">
+									<?
+										$hsflag_patients_Query = "SELECT SharedHealthFlag FROM Patient WHERE patientID=$patientID";
+										$result = mysqli_query($connection,$hsflag_patients_Query);
+										$row = $result->fetch_assoc();
+										if($row["SharedHealthFlag"]=='Y')
+											echo "Yes";
+										else
+											echo "No";
+									?>
+									</div>
+                                    <div>Health Summary Exists?</div>
                                 </div>
                             </div>
                         </div>
                         <a href="#">
                             <div class="panel-footer">
-                                <span class="pull-left">Total Discharges</span>
+                                <span class="pull-left">Health Summary Exists?</span>
                                 <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
                                 <div class="clearfix"></div>
                             </div>

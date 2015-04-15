afe89b65bd424cf6113a1c79036cb62f003f87b8
shiv1994 <shivramx@gmail.com>
2015-04-15 02:15:55 -0400

    Fixed back patient dashboard.

--- pages/view_health_summary.php
+++ pages/view_health_summary.php
@@ -42,8 +42,16 @@
 	}
 ?>
 <!DOCTYPE html>
+<html lang="en">
+
 <html>
 	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+	
 		<title> View Health Summary </title>
         <!-- Bootstrap Core CSS -->
         <link href="../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
